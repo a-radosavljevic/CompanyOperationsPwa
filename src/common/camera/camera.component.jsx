a710ae0252cbd474eaf2c65089df3a169a4d96c9
Александар Радосављевић <a.radosavljevic96@outlook.com>
2023-07-31 08:50:05 +0200

    Zatvaranje kamere

--- src/common/camera/camera.component.jsx
+++ src/common/camera/camera.component.jsx
@@ -7,17 +7,12 @@ const Camera = ({ handleTakingPhoto }) => {
     const [stream, setStream] = useState(false)
     const cameraRef = useRef(null);
     const canvasRef = useRef(null);
+    const tracks = useRef(null);
 
     useEffect(() => {
         startCamera();
         return () => {
-            if (cameraRef.current) {
-                // zatvaranje strimaova za kameru nakon gasenja komponente
-                stream && stream.getTracks().forEach(function (track) {
-                    track.stop();
-                });
-                cameraRef.current.srcObject = null;
-            }
+            closeCamera();
         }
     }, [])
 
@@ -26,14 +21,25 @@ const Camera = ({ handleTakingPhoto }) => {
             if (cameraRef.current) {
                 cameraRef.current.srcObject = stream;
             }
+            tracks.current = stream.getTracks();
         }
     }, [stream])
 
     const startCamera = async () => {
+        setPreview(false);
         try {
-            setStream(await navigator.mediaDevices.getUserMedia({ video: true }));
+            setStream(await navigator.mediaDevices.getUserMedia({ video: true }))
         } catch (err) {
             console.error(err);
+        }
+    }
+
+    const closeCamera = () => {
+        tracks.current.forEach(function (track) {
+            track.stop();
+        });
+        if (cameraRef.current) {
+            cameraRef.current.srcObject = null;
         }
     }
 
@@ -47,6 +53,7 @@ const Camera = ({ handleTakingPhoto }) => {
             const photo = dataURLToByteArray(photoDataUrl);
             setPreview(true);
             handleTakingPhoto(photo, 'jpg');
+            closeCamera();
         }
     }
 
@@ -55,7 +62,7 @@ const Camera = ({ handleTakingPhoto }) => {
             <video name='content' ref={cameraRef} style={{ display: preview ? 'none' : 'block' }} autoPlay playsInline></video>
             <canvas ref={canvasRef} style={{ display: preview ? 'block' : 'none' }}></canvas>
             {!preview && <button className="btn btn-outline-primary" onClick={takePhoto}>Fotografiši</button>}
-            {preview && <button className="btn btn-outline-primary" onClick={() => setPreview(false)}>Fotografiši ponovo</button>}
+            {preview && <button className="btn btn-outline-primary" onClick={startCamera}>Fotografiši ponovo</button>}
         </>
     )
 }
